860ba815363f1a75cf3d1fb6bcb879339ef982ba
charviadhyam <charvi.adhyam2024@vitstudent.ac.in>
2026-01-20 23:57:09 +0530

    Update potentiometer_LED.cpp

--- potentiometer_LED.cpp
+++ potentiometer_LED.cpp
@@ -8,9 +8,11 @@ void setup() {
 void loop() {
   pot=analogRead(34);
   Serial.println(pot);
-  if (pot>100){
+  if (pot>1000){
     digitalWrite(13, HIGH);
     delay(200);
+  }
+  else {
     digitalWrite(13, LOW);
     delay(200);
   }
